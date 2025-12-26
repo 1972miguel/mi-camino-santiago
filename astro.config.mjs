830fca741818a30208be541bcc3929719f663569
1972miguel <miguelai1972@gmail.com>
2025-12-26 15:43:05 +0100

    creación de galería

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mi-camino-santiago.vercel.app', // Cambia esto cuando tengas tu dominio o deja así por ahora
+  // Cambia esto cuando tengas tu dominio o deja así por ahora
+  site: 'https://mi-camino-santiago.vercel.app',
+
+  integrations: [mdx()]
 });
